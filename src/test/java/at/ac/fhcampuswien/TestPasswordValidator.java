aa875fa286c4f1d88012b8ca3d3ce0bb3402661d
flo19 <flo.mayerhofe99@gmail.com>
2021-06-02 17:14:22 +0200

    test cases updated

--- src/test/java/at/ac/fhcampuswien/TestPasswordValidator.java
+++ src/test/java/at/ac/fhcampuswien/TestPasswordValidator.java
@@ -16,12 +16,34 @@ public class TestPasswordValidator {
     }
 
     @Test
+    @DisplayName("Password not null")
+    public void testPasswordNotNull() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String password = "Ppa12s(ss";
+        assertTrue(passwordValidator.checkPassword(password));
+    }
+
+
+
+
+   @Test
     @DisplayName("Password length < 8")
     public void testPasswordLengthShort() {
         PasswordValidator passwordValidator = new PasswordValidator();
         String password = "fdfsd";
         assertFalse(passwordValidator.checkMinLength(password));
     }
+
+    @Test
+    @DisplayName("Password length > 8")
+    public void testPasswordLengthNotShort() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String password = "fdfsdkukrt";
+        assertTrue(passwordValidator.checkMinLength(password));
+    }
+
+
+
 
     @Test
     @DisplayName("Password length > 25")
@@ -32,36 +54,91 @@ public class TestPasswordValidator {
     }
 
     @Test
-    @DisplayName("Password contains capital")
-    public void testPasswordCapital() {
+    @DisplayName("Password length < 25")
+    public void testPasswordLengthNotLong() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String password = "dfdsopksdpkagüa";
+        assertTrue(passwordValidator.checkMaxLength(password));
+    }
+
+
+
+
+    @Test
+    @DisplayName("Password contains no capital")
+    public void testPasswordNoCapital() {
         PasswordValidator passwordValidator = new PasswordValidator();
         String password = "ab8/cdefghjkl";
         assertFalse(passwordValidator.checkCapital(password));
     }
 
     @Test
-    @DisplayName("Password contains number")
-    public void testPasswordNumber() {
+    @DisplayName("Password contains capital")
+    public void testPasswordCapital() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String password = "ab8/cdEfghjkl";
+        assertTrue(passwordValidator.checkCapital(password));
+    }
+
+
+
+
+    @Test
+    @DisplayName("Password contains no number")
+    public void testPasswordNoNumber() {
         PasswordValidator passwordValidator = new PasswordValidator();
         String password = "Aa/bcdefghjkl";
         assertFalse(passwordValidator.checkNumber(password));
     }
 
     @Test
-    @DisplayName("Password contains special char")
-    public void testPasswordSpecialChar() {
+    @DisplayName("Password contains number")
+    public void testPasswordNumber() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String password = "Aa/bcdefgh4jkl";
+        assertTrue(passwordValidator.checkNumber(password));
+    }
+
+
+
+
+    @Test
+    @DisplayName("Password contains no special char")
+    public void testPasswordNoSpecialChar() {
         PasswordValidator passwordValidator = new PasswordValidator();
         String password = "A6bcdefghjkl";
         assertFalse(passwordValidator.checkSpecialChar(password));
     }
 
     @Test
-    @DisplayName("Password contains ongoing numbers")
-    public void testPasswordTwoNumbersInRow() {
+    @DisplayName("Password contains special char")
+    public void testPasswordSpecialChar() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String password = "A6bcdefgh/jkl";
+        assertTrue(passwordValidator.checkSpecialChar(password));
+    }
+
+
+
+
+    @Test
+    @DisplayName("Password doesn't contain ongoing numbers")
+    public void testPasswordNoTwoNumbersInRow() {
         PasswordValidator passwordValidator = new PasswordValidator();
         String password = "A/234abcdefghjkl";
         assertFalse(passwordValidator.checkContinueNumbers(password));
     }
+
+    @Test
+    @DisplayName("Password contains ongoing numbers")
+    public void testPasswordTwoNumbersInRow() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String password = "A/22abcdefghjkl";
+        assertTrue(passwordValidator.checkContinueNumbers(password));
+    }
+
+
+
 
     @Test
     @DisplayName("Password contains the same number four times in a row")
@@ -69,5 +146,13 @@ public class TestPasswordValidator {
         PasswordValidator passwordValidator = new PasswordValidator();
         String password = "A/2222abcdefghjkl";
         assertFalse(passwordValidator.checkSameNumberInARow(password));
+    }
+
+    @Test
+    @DisplayName("Password doesn't contain the same number four times in a row")
+    public void testPasswordNoFourNumbersInRow() {
+        PasswordValidator passwordValidator = new PasswordValidator();
+        String password = "A/222abcdefghjkl";
+        assertTrue(passwordValidator.checkSameNumberInARow(password));
     }
 }
